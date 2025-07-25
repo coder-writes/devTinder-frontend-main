e7e92c00614aeabb32e6e0626b60e01e6a8cc843
Rishi Verma <vrishi7654@gmail.com>
2025-07-25 17:58:11 +0530

    some changes

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,15 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  
-  plugins: [react(),
-    tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
   build: {
     chunkSizeWarningLimit: 1500,
+  },
+  server: {
+    historyApiFallback: true,
+  },
+  preview: {
+    port: 3000,
+    strictPort: true,
   },
 })
